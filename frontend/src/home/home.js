2a92a27739c3dbeb6957b16bf5bcd6a3510f138d
LBartolini <l.bartolini02@gmail.com>
2021-06-08 15:51:51 +0200

    aggiunti radio button per scegliere locale o remoto

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -1,13 +1,20 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {UserContext} from '@src/UserContext.js';
 import {
 	useHistory,
   } from "react-router-dom";
+import GlobalVar from '@src/GlobalVar.js';
 import './home.css';
 
 const HomeScreen = () => {
 	const history = useHistory();
 	const {user, setUser} = useContext(UserContext);
+
+	const checkUrlLocale = GlobalVar.urlAPI === "http://192.168.1.10/atmi-api";
+	const checkUrlRemoto = GlobalVar.urlAPI === "http://lbartolini.ddns.net:3000/atmi-api";
+
+	const [urlLocale, setUrlLocale] = useState(checkUrlLocale);
+	const [urlRemoto, setUrlRemoto] = useState(checkUrlRemoto);
 	// per cambiare solo un attributo dello stato setUser({...stato, attr: new_value})
 	return (
 		<div className="home router-content">
@@ -18,6 +25,20 @@ const HomeScreen = () => {
 				<h6>Appalti e bandi di manutenzione</h6>
 				<br/>
 				<h6>Eseguire l'accesso per entrare nel portale</h6>
+			</div>
+			<div className="radio-buttons">
+				<div>URL LOCALE <input type="radio" value="locale" checked={urlLocale} 
+				onChange={() => { 
+					setUrlLocale(true);
+					setUrlRemoto(false);
+					GlobalVar.urlAPI = "http://192.168.1.10/atmi-api";
+				 }}/></div>
+				<div>URL REMOTO <input type="radio" value="remoto" checked={urlRemoto}
+				onChange={() => { 
+					setUrlRemoto(true);
+					setUrlLocale(false);
+					GlobalVar.urlAPI = "http://lbartolini.ddns.net:3000/atmi-api";
+				 }}/></div>
 			</div>
 		</div>
 	);
